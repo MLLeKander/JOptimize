11f3d4d45a3780c5ca7a3f9e23028989aa87df7e
Michael LeKander <m.l.lekander@gmail.com>
2015-08-18 22:20:05 -0700

    MultiBGD

--- src/nl/rug/joptimize/opt/optimizers/MultiBGD.java
+++ src/nl/rug/joptimize/opt/optimizers/MultiBGD.java
@@ -1,5 +1,7 @@
 package nl.rug.joptimize.opt.optimizers;
 
+import java.util.Collection;
+
 import nl.rug.joptimize.opt.AbstractOptimizer;
 import nl.rug.joptimize.opt.OptParam;
 import nl.rug.joptimize.opt.SeparableCostFunction;
@@ -7,6 +9,15 @@ import nl.rug.joptimize.opt.SeparableCostFunction;
 public class MultiBGD<ParamType extends OptParam<ParamType>> extends
         AbstractOptimizer<ParamType> {
     protected double[] learningRates;
+    
+    public MultiBGD(Collection<Double> learningRates, double epsilon, int tMax) {
+        super(epsilon, tMax);
+        this.learningRates = new double[learningRates.size()];
+        int i = 0;
+        for (Double d : learningRates) {
+            this.learningRates[i++] = d;
+        }
+    }
 
     public MultiBGD(double[] learningRates, double epsilon, int tMax) {
         super(epsilon, tMax);
@@ -18,14 +29,18 @@ public class MultiBGD<ParamType extends OptParam<ParamType>> extends
         ParamType grad = cf.deriv(params);
         ParamType minParams = null;
         double minErr = Double.MAX_VALUE;
+        double minRate = 0;
         for (double learningRate : learningRates) {
-            ParamType tmpParams = grad.multiply_s(-learningRate).add_s(params);
+            ParamType tmpParams = grad.multiply(-learningRate).add_s(params);
             double tmpErr = cf.error(tmpParams);
             if (tmpErr <= minErr) {
                 minParams = tmpParams;
                 minErr = tmpErr;
+                minRate = learningRate;
             }
+            //System.out.printf("%.2f:%.4f ",learningRate,tmpErr);
         }
+        System.out.println("\nminRate: "+minRate);
         return minParams;
     }
 }
